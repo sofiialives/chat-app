11c3927a9cfec2a6a22c8aebf3748b9448c4bb67
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-05-02 17:13:21 +0200

    not selected chat component

--- frontend/src/components/messages/MessageContainer.tsx
+++ frontend/src/components/messages/MessageContainer.tsx
@@ -1,17 +1,25 @@
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
+import WelcomeComponents from "./WelcomeComponents";
 
 interface MessageContainerProps {}
 
 export default function MessageContainer({}: MessageContainerProps) {
+  const welcome = true;
   return (
     <div className="tablet:w-[480px] flex flex-col">
-      <div className="bg-gray-300 px-4 tablet:px-6 py-2 mb-2">
-        <span className="label-text">To: </span>
-        <span className="label-text">Sofiika Karpova</span>
-      </div>
-      <Messages />
-      <MessageInput />
+      {welcome ? (
+        <WelcomeComponents />
+      ) : (
+        <>
+          <div className="bg-gray-300 px-4 tablet:px-6 py-2 mb-2">
+            <span className="label-text">To: </span>
+            <span className="label-text">Sofiika Karpova</span>
+          </div>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
     </div>
   );
 }
