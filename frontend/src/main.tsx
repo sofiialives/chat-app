184ecdd521f249d2a698b4825ddb64769136bce0
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-05-03 11:38:38 +0200

    auth context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import AuthContextProvider from "./context/authContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
