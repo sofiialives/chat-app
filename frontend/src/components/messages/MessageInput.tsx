63b7f4efc92173c8baaa944a9e2471d87ea06ad7
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-05-06 20:46:11 +0200

    emoji input message

--- frontend/src/components/messages/MessageInput.tsx
+++ frontend/src/components/messages/MessageInput.tsx
@@ -3,11 +3,14 @@ import Button from "../reusable/Button";
 import TextField from "../reusable/TextField";
 import { useState } from "react";
 import useSendMessage from "../../hooks/useSendMessage";
+import EmojiPicker from "emoji-picker-react";
+import { RiEmojiStickerLine } from "react-icons/ri";
 
 interface MessageInputProps {}
 
 export default function MessageInput({}: MessageInputProps) {
   const [message, setMessage] = useState("");
+  const [openEmoji, setOpenEmoji] = useState(false);
   const { loading, sendMessage } = useSendMessage();
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
@@ -17,6 +20,8 @@ export default function MessageInput({}: MessageInputProps) {
     setMessage("");
   };
 
+  const handleEmojiClick = ({ emoji }: { emoji: string }) =>
+    setMessage((prevMessage) => `${prevMessage}${emoji}`);
   return (
     <form className="px-4 my-3" onSubmit={handleSubmit}>
       <div className="w-full relative">
@@ -26,6 +31,14 @@ export default function MessageInput({}: MessageInputProps) {
           value={message}
           onChange={(e) => setMessage(e.target.value)}
         />
+        <Button
+          type="button"
+          variant="message"
+          onClick={() => setOpenEmoji(!openEmoji)}
+          className="mr-10"
+        >
+          <RiEmojiStickerLine className="w-4 h-4 tablet:w-6 tablet:h-6 text-gray-300" />
+        </Button>
         <Button type="submit" variant="message">
           {loading ? (
             <span className="loading loading-spinner text-primary" />
@@ -37,6 +50,11 @@ export default function MessageInput({}: MessageInputProps) {
           )}
         </Button>
       </div>
+      {openEmoji && (
+        <div className="absolute top-0 left-0 z-1">
+          <EmojiPicker onEmojiClick={handleEmojiClick} />
+        </div>
+      )}
     </form>
   );
 }
