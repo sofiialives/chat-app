7c389187945576b8e1154927847f0ae1ea8402e6
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-05-09 13:32:09 +0200

    header styles

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {}
 export default function Header({}: HeaderProps) {
   const { authUser } = useAuthContext();
   return (
-    <Container className="flex justify-between items-center">
+    <Container className="flex justify-between items-center py-3">
       <div>Icon</div>
       <div className="flex items-center gap-2">
         <h1 className="text-3xl font-medium text-gray-200">
@@ -17,11 +17,12 @@ export default function Header({}: HeaderProps) {
         <img
           src={authUser?.profilePicture}
           alt="avatar"
-          width={48}
-          height={48}
+          width={40}
+          height={40}
+          className="tablet:w-12 tablet:h-12"
         />
         <Link to="/settings">
-          <IoSettingsOutline className="w-12 h-12" />
+          <IoSettingsOutline className="w-10 h-10 tablet:w-12 tablet:h-12 text-gray-300" />
         </Link>
       </div>
     </Container>
