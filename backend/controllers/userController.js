ab56ca19976c0ec489586926caaf0438d3b13284
sofiialives <127687828+sofiialives@users.noreply.github.com>
2024-05-09 20:42:55 +0200

    updateInfo controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import { User } from "../db/models/userModel.js";
+import { httpError } from "../helpers/httpError.js";
 
 export const getUsersForSidebar = async (req, res) => {
   const { _id: loggedInUserId } = req.user;
@@ -8,4 +9,45 @@ export const getUsersForSidebar = async (req, res) => {
   );
 
   return res.json(filterUsers);
+};
+
+export const updateUserInfo = async (req, res) => {
+  const { _id } = req.user;
+  const { gender, username, password, newPassword } = req.body;
+
+  let updatedUser;
+
+  if (password && newPassword) {
+    if (password === newPassword) throw httpError(401);
+
+    const user = await User.findById({ _id });
+    if (!user) throw httpError(401);
+
+    updatedUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        username,
+        gender,
+        password: newPassword,
+      },
+      { new: true }
+    );
+
+    await updatedUser.hashPassword();
+    await updatedUser.save();
+  } else {
+    updatedUser = await User.findByIdAndUpdate(
+      _id,
+      { username, gender },
+      { new: true }
+    );
+    if (!updatedUser) throw httpError(404);
+  }
+
+  res.json({
+    user: {
+      username: updatedUser.username,
+      gender: updatedUser.gender,
+    },
+  });
 };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
 import authenticate from "../middleware/authenticate.js";
-import { getUsersForSidebar } from "../controllers/userController.js";
+import {
+  getUsersForSidebar,
+  updateUserInfo,
+} from "../controllers/userController.js";
 import { ctrlWrapper } from "../helpers/ctrlWrapper.js";
 const router = express.Router();
 
 router.get("/", authenticate, ctrlWrapper(getUsersForSidebar));
+router.patch("/", authenticate, ctrlWrapper(updateUserInfo));
 
 export default router;
